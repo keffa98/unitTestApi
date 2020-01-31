76dd28e4dc89e84e90463b9768850d9b2264aae4
Slade0290 <ly.david@live.fr>
2020-01-31 12:43:33 +0100

    test intégration

--- testUnitaireApi/src/test/book.js
+++ testUnitaireApi/src/test/book.js
@@ -15,3 +15,22 @@ chai.use(chaiAsPromised);
 // tout les packages et fonction nescessaire au test sont importé ici, bon courage
 
 // fait les Tests d'integration en premier
+
+  describe('get /book', () => {
+    beforeEach(() => {
+    	resetDatabase(path.join(__dirname, '../data/books.json'), {
+    		books : []
+    	});
+    });
+
+    it('response body is an object', (done) => {
+		chai.request(server)
+			.get('/book')  
+			.end(function (err, res) {
+			     expect(res.body).to.be.an('object');
+			     done();
+			  });
+    });
+
+
+  });
